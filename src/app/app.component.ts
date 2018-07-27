021f0ced655c7e0483084f644cecb18a9350f1fc
Franck2509 <franck97@bluewin.ch>
2018-07-27 09:16:42 +0200

    multi language done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import * as jsPDF from 'jspdf';
 
 @Component({
   selector: 'tr-root',
@@ -6,5 +8,68 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'traduction';
+  user = {
+    name: 'Arthur',
+    age: 42
+  };
+  valueCheck: string;
+  score: number;
+  total: number;
+  amount: number;
+  text = [];
+
+  selectScales = [
+    {value: 'null', display: 'Not Applicable'},
+    {value: '0', display: 'Weak'},
+    {value: '50', display: 'Moderate'},
+    {value: '100', display: 'Strong'},
+    {value: '150', display: 'Outstanding'}
+  ];
+
+  constructor(private translate: TranslateService) {
+    translate.setDefaultLang('en');
+
+  }
+
+  switchLanguage(language: string) {
+    this.translate.use(language);
+  }
+
+  value() {
+    this.text = ["Value Check"]
+    this.total = 0;
+    this.amount = 0;
+    this.valueCheck = (<HTMLInputElement>document.getElementById('vc')).innerHTML;
+    if (((<HTMLInputElement>document.getElementById('valueq1')).value) !== 'null') {
+      this.total += parseInt((<HTMLInputElement>document.getElementById('valueq1')).value);
+      this.amount += 1;
+    }
+    this.text.push((<HTMLInputElement>document.getElementById('vq1')).innerHTML + ": "+ (<HTMLInputElement>document.getElementById('valueq1')).value);
+    this.text.push((<HTMLInputElement>document.getElementById('textq1')).value);
+
+    if (((<HTMLInputElement>document.getElementById('valueq2')).value) !== 'null') {
+      this.total += parseInt((<HTMLInputElement>document.getElementById('valueq2')).value);
+      this.amount += 1;
+    }
+    this.text.push((<HTMLInputElement>document.getElementById('vq2')).innerHTML + ": "+(<HTMLInputElement>document.getElementById('valueq2')).value);
+    this.text.push((<HTMLInputElement>document.getElementById('textq2')).value);
+
+    if (((<HTMLInputElement>document.getElementById('valueq3')).value) !== 'null') {
+      this.total += parseInt((<HTMLInputElement>document.getElementById('valueq3')).value);
+      this.amount += 1;
+    }
+    this.text.push((<HTMLInputElement>document.getElementById('vq3')).innerHTML + ": "+ (<HTMLInputElement>document.getElementById('valueq3')).value);
+    this.text.push((<HTMLInputElement>document.getElementById('textq3')).value);
+
+    this.score = this.total / this.amount;
+    this.text.push("Score: " + this.score.toString())
+  }
+
+  savePDF(){
+    this.value();
+    const doc = new jsPDF();
+    doc.text(this.text,10,10);
+    doc.save("prototyp.pdf");
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BootstrapComponent } from './bootstrap/bootstrap.component';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BootstrapComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }),
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
